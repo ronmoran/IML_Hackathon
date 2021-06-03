68b87fb20c66398be18fe484273e4f81cb448f1f
Ron Moran <nomail@nomail.com>
2021-06-04 02:27:01 +0300

    with meaningful numerical values instead of text

--- classifier.py
+++ classifier.py
@@ -7,37 +7,67 @@ crimes_dict = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE', 3: 'DECEPTIVE PRA
 crimes_inv_dict = {v: k for k, v in crimes_dict.items()}  # todo return crimes and not label
 LOCATION_DESCRIPTION = {}
 LOCATION_DESCRIPTION_COL = "Location Description"
+labels_col = "Primary Type"
+COLS_FOR_FREQ = [
+                 LOCATION_DESCRIPTION_COL]
 
 
 def pre_processing(X: pd.DataFrame):
-    processed_data = X[['Date', 'Arrest', 'Domestic', 'X Coordinate',
-                        'Y Coordinate', 'Updated On', "Beat",
-                        LOCATION_DESCRIPTION_COL]].copy()
 
+    processed_data = X[['Date', 'Arrest', 'Domestic', 'X Coordinate',
+                        'Y Coordinate', 'Updated On', "Beat", "District",
+                        "Ward", "Community Area",
+                        LOCATION_DESCRIPTION_COL]].copy()
     processed_data['Date'] = pd.to_datetime(processed_data['Date'], format='%m/%d/%Y %I:%M:%S %p')
     processed_data['Updated On'] = pd.to_datetime(processed_data['Updated On'], format='%m/%d/%Y %I:%M:%S %p')
     processed_data['Time diff'] = processed_data['Updated On'] - processed_data['Date']
-    processed_data[LOCATION_DESCRIPTION_COL] = processed_data[
-        LOCATION_DESCRIPTION_COL].apply(lambda x: LOCATION_DESCRIPTION.get(
-        x, -1))  # todo why -1? what to do with newly found data
     processed_data['Time diff'] = processed_data['Time diff'].apply(lambda x: x.total_seconds())
     processed_data['Hour'] = processed_data['Date'].apply(lambda x: x.time().hour)
     processed_data['Weekday'] = processed_data['Date'].apply(lambda x: x.date().weekday())
-    processed_data = pd.concat((processed_data, pd.get_dummies(processed_data['Weekday'],
-                                               prefix="weekday_")), 1)
-
-    processed_data.drop(['Date', 'Updated On', "Weekday"], axis=1,
-                        inplace=True)
+    for col in COLS_FOR_FREQ:
+        tab = pd.read_pickle(f"{col}.pkl")
+        to_append: pd.DataFrame = tab.loc[processed_data[col].values, :]
+        processed_data.loc[:, to_append.columns.values] = to_append.values
+    # tab = pd.read_pickle(f"{LOCATION_DESCRIPTION_COL}.pkl")
+    # words = processed_data[LOCATION_DESCRIPTION_COL].str.findall("["
+    #                                                              "A-Za-z0-9]+")
+    processed_data.drop(['Date', 'Updated On', *COLS_FOR_FREQ,
+                         LOCATION_DESCRIPTION_COL],
+                        axis=1, inplace=True)
     return processed_data
+
+
+def get_col_freq(col: str, labels_col:str, data: pd.DataFrame) -> pd.DataFrame:
+    freq = pd.crosstab(data[col], data[labels_col])
+    freq:pd.DataFrame = freq.div(freq.sum(axis=1), axis=0)
+    freq.rename(columns={label_name: f"{col}_{label_name}" for label_name in
+                         freq.columns}, inplace=True)
+    return freq
+
+
+def get_word_freq(col: pd.Series, labels:pd.Series) -> pd.DataFrame:
+    words = col.str.findall("[A-Za-z0-9]+")
+    cols = [f"{LOCATION_DESCRIPTION_COL}_{label}" for label
+            in labels.unique()]
+    freq_tab = pd.DataFrame(columns=cols)
+    for row, label in zip(words, labels):
+        for word in row:
+            if word not in freq_tab.index:
+                freq_tab.loc[word] = pd.Series(index=cols,
+                                               data=[0 for _ in cols])
+            freq_tab.loc[word, f"{LOCATION_DESCRIPTION_COL}_{label}"] += 1
+    return freq_tab.div(freq_tab.sum(axis=1), axis=0)
 
 
 def train_pre_process(X: pd.DataFrame):
     processed_data = X.dropna()
     y_train = processed_data['Primary Type'].apply(lambda x: crimes_inv_dict[x])
-    global LOCATION_DESCRIPTION
-    if LOCATION_DESCRIPTION == {}:
-        LOCATION_DESCRIPTION = {val: i for i, val in enumerate(set(
-                processed_data[LOCATION_DESCRIPTION_COL]))}
+    for col in COLS_FOR_FREQ:
+        tab = get_col_freq(col, labels_col, processed_data)
+        tab.to_pickle(f"{col}.pkl")
+    # word_freq = get_word_freq(processed_data[LOCATION_DESCRIPTION_COL],
+    #                           y_train)
+    # word_freq.to_pickle(f"{LOCATION_DESCRIPTION_COL}.pkl")
     return pre_processing(processed_data), y_train
 
 
